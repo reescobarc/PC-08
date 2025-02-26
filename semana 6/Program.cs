cf0672e040ad470e68009b20f96b87defaf69439
reescobarc <reescobarc@correo.url.edu.gt>
2025-02-25 21:02:29 -0600

    Add files via upload

---
+++ semana 6/Program.cs
@@ -0,0 +1,36 @@
+﻿class PrimerParcial
+{
+    static void Main(string [] args)
+    {
+        Console.WriteLine("Ingrese el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el tercer número: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        if (num1 %2 == 0)
+        {
+            Console.WriteLine("El número "+ num1 + " es par.");
+        }
+        else {
+            Console.WriteLine("El número "+ num1 + " es impar.");
+        }
+
+        if (num2 %2 == 0)
+        {
+            Console.WriteLine("El número "+ num2 + " es par.");
+        }
+        else {
+            Console.WriteLine("El número "+ num2 + " es impar.");
+        }
+
+        if (num3 %2 == 0)
+        {
+            Console.WriteLine("El número "+ num3 + " es par.");
+        }
+        else {
+            Console.WriteLine("El número "+ num3 + " es impar.");
+        }
+    }
+}
